ea5dbe232fc35f1e9a0ef7cc140fe45e422cdd3a
Mostafadars <92469012+Mostafadars@users.noreply.github.com>
2022-11-25 20:18:15 +0200

    last version

--- main.cpp
+++ main.cpp
@@ -6,4 +6,13 @@ int main()
     BankingApplication app;
     app.run();
     return 0;
+
+    // case 1
+    // Basic, Balance = 7000
+
+    // case 2
+    // Basic, Balance = 5000
+
+    // case 3
+    // Saving, Balance = 3000
 }
